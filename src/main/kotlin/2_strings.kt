18a5e8a5991437e31eb4acbe41cdc323cecc4110
Anartz Mugika <amugika@codesyntax.com>
2017-05-29 08:23:51 +0200

    Add new strings options

--- src/main/kotlin/2_strings.kt
+++ src/main/kotlin/2_strings.kt
@@ -1,17 +1,19 @@
-/**
+/***********************************************************
  * Created by Anartz Mugika on 29/05/2017.
  */
 
 fun main(args: Array<String>) {
+    //If use 'var', variable is mutable (let in typescript)
+    //If use 'val' not mutable = constant
     val name = "Anartz"
 
     //Print name value
-    println("My name is " + name)
+    printValue("My name is " + name)
 
     //Print float value with two decimals
     val pi = 3.14159265358979323
-    val s = "pi = %.2f".format(pi)
-    println(s);
+    var s = "pi = %.2f".format(pi)
+    printValue(s);
 
     var value1 = 1
     var value2 = "2"
@@ -19,8 +21,28 @@ fun main(args: Array<String>) {
     var value4 = 4.0
 
     //Use java string forma
-    println(java.lang.String.format("%d, %s, %6f : %3f", value1, value2, value3, value4))
+    printValue(java.lang.String.format("%d, %s, %6f : %3f", value1, value2, value3, value4))
 
     //Use kotlin
-    println("%d, %s, %6f : %3f".format(value1, value2, value3, value4))
+    printValue("%d, %s, %6f : %3f".format(value1, value2, value3, value4))
+
+    //String templates
+    val i = 10
+    var str = "i = $i" // evaluates to "i = 10"
+    printValue(str)
+
+    s = "abc"
+    str = "$s.length is ${s.length}" // evaluates to "abc.length is 3"
+
+    printValue(str)
+
+    //Literal string always inside"{'string'}
+    val price = """
+        ${'$'}9.99
+        """
+    printValue(price)
+}
+
+fun printValue(x: String) {
+    println(x)
 }
